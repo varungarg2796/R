caf2252514ffd033e4f1b3bd186db194f55b8437
varungarg2796 <varungarg2796@hotmail.com>
2018-09-18 02:08:00 +0100

    functions and vectors

---
+++ functions.R
@@ -0,0 +1,21 @@
+rnorm(n=5,mean=10,sd=8)
+#62.8% of the numbers will lie between 2 and 18 (law of large numbers)
+
+# ?rnorm() --> will help you with the function
+
+seq(from=10,to=20,by=3)
+
+
+#Get a 100 numbers between 10 to 20
+seq(from=10,to=20,length.out = 100)
+
+#identical length as the vector with along with
+x <- c(1,3)
+seq(from=10,to=20,along.with = x)
+
+
+rep(5:6,times=5)
+rep(5:6,each=5)
+
+#get help for function c
+?c()
